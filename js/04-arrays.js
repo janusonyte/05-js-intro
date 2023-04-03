2d04edc169aa58c6bf72fce91706400b3716f1eb
janusonyte <janusonytegabriele@gmail.com>
2023-04-03 19:59:09 +0100

    arrays, typeof

---
+++ js/04-arrays.js
@@ -0,0 +1,8 @@
+console.clear();
+const pazymiai = [10,2,8,4,6,3,5];
+
+console.log(pazymiai);
+const paskutinioIndexas = pazymiai.length-1;
+console.log('Pazymiu kiekis:', pazymiai.length);
+console.log('Paskutinis pazymys:', pazymiai[paskutinioIndexas]);
+console.log('Paskutinis pazymys:', pazymiai[pazymiai.length-1]);
---
+++ js/05-typeof.js
@@ -0,0 +1,17 @@
+console.clear();
+
+/*
+
+Type of - duomens tipo nustatymas
+
+*/
+
+const a = 5;
+const aType = typeof a;
+
+console.log(aType);
+
+const d = [];
+const dType = typeof d;
+
+console.log(dType);
